b7e55734e25894e22db41deaae31d135cce94505
phamhugducBacoor <phamhungduc@bacoor.co>
2024-12-12 14:33:58 +0700

    Add bacoorpasskeywallet configuration and update URL_PASSKEY in constants

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -5,14 +5,21 @@ export const infoGroup = {
     icon: 'https://ipfs.pantograph.app/ipfs/QmUn7MnyA9HWfHNRG7oLTweNG93ztaErFdcg36Y2uxENUu?filename=noti.png',
     rdns: 'co.bacoor.keyring',
   },
+  bacoorpasskeywallet: {
+    uuid: '123e4567-e89b-12d3-a456-426614174000',
+    name: 'Eggle Gamewallet',
+    icon: 'https://ipfs.pantograph.app/ipfs/QmUn7MnyA9HWfHNRG7oLTweNG93ztaErFdcg36Y2uxENUu?filename=noti.png',
+    rdns: 'co.bacoor.keyring',
+  },
 
 }
 
 export const GROUP_SLUG = 'egglegamewallet'
+// export const GROUP_SLUG = 'bacoorpasskeywallet'
 // export const GROUP_SLUG = 'keyringpasskeywallet'
 
 export const STORAGE_KEY = {
   ACCOUNT_PASSKEY: 'ACCOUNT_PASSKEY_' + GROUP_SLUG,
 }
-export const URL_PASSKEY = 'https://pass.w3w.app'
+export const URL_PASSKEY = 'https://pass.keyring.app'
 // export const URL_PASSKEY = 'http://localhost:3000'
