ef1e9ab32f67e46eb378f10673930e39ed9b680f
phamhugducBacoor <phamhungduc@bacoor.co>
2024-12-13 15:47:36 +0700

    Update README and constants for passkeywallet integration

--- README.md
+++ README.md
@@ -6,20 +6,20 @@
 ## Install (for single position)
 
 ```bash
-npm install --save sdk-v2-egglegamewallet
+npm install --save sdk-v2-passkeywallet
 
 
 ```
 
 ```bash
-yarn add sdk-v2-egglegamewallet
+yarn add sdk-v2-passkeywallet
 ```
 
 ## Usage
 ```jsx
 //Used for the first file to run (index.js)
 import React from "react";
-import { PasskeyProvider } from "sdk-v2-egglegamewallet";
+import { PasskeyProvider } from "sdk-v2-passkeywallet";
 
 const App = ({children}) => {
 
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -11,15 +11,22 @@ export const infoGroup = {
     icon: 'https://ipfs.pantograph.app/ipfs/QmUn7MnyA9HWfHNRG7oLTweNG93ztaErFdcg36Y2uxENUu?filename=noti.png',
     rdns: 'co.bacoor.keyring',
   },
+  passkeywallet: {
+    uuid: '123e4567-e89b-12d3-a456-426614174000',
+    name: 'KEYRING SMART',
+    icon: 'https://ipfs.pantograph.app/ipfs/QmWdV4p9FvAUKEnGHefwsYxUrtY8qyEX61xbHdGXworS8k?filename=icon%20NOTI.png',
+    rdns: 'co.bacoor.keyring',
+  },
 
 }
 
-export const GROUP_SLUG = 'egglegamewallet'
+// export const GROUP_SLUG = 'egglegamewallet'
+export const GROUP_SLUG = 'passkeywallet'
 // export const GROUP_SLUG = 'bacoorpasskeywallet'
 // export const GROUP_SLUG = 'keyringpasskeywallet'
 
 export const STORAGE_KEY = {
   ACCOUNT_PASSKEY: 'ACCOUNT_PASSKEY_' + GROUP_SLUG,
 }
-export const URL_PASSKEY = 'https://pass.w3w.app'
+export const URL_PASSKEY = 'https://smart.keyring.app'
 // export const URL_PASSKEY = 'http://localhost:3000'
