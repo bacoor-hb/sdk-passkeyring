d8517ad45e4f17f513e584fb3b185cc2b0d881fc
phamhugducBacoor <phamhungduc@bacoor.co>
2024-12-23 17:09:52 +0700

    Add comment to restrict type modifications in SlugGroup for build script consistency

--- lib/constants/type.ts
+++ lib/constants/type.ts
@@ -1,3 +1,4 @@
+// must not add another type in this file, only add slug into SlugGroup=> because it related to file build.js, multi-build.js
 export type SlugGroup =
   | 'egglegamewallet'
   | 'egglepasskeywallet'
