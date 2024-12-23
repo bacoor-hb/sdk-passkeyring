a791ea03b85ad5224bff0fec9afc0eb582a2c5ec
phamhugducBacoor <phamhungduc@bacoor.co>
2024-12-23 15:55:45 +0700

    Update build script to replace groupSlug and package name references for consistency

--- build.js
+++ build.js
@@ -3,7 +3,6 @@ const path = require('path')
 
 // Bước 1: Thay thế groupSlug trong file constants/index.ts
 const constantsFilePath = path.join(__dirname, 'lib/constants/index.ts')
-const oldGroupSlug = 'egglepasskeywallet-decard'
 const newGroupSlug = 'egglepasskeywallet'
 
 fs.readFile(constantsFilePath, 'utf8', (err, data) => {
@@ -12,7 +11,7 @@ fs.readFile(constantsFilePath, 'utf8', (err, data) => {
     return
   }
 
-  const updatedData = data.replace(`export const GROUP_SLUG: SlugGroup = '${oldGroupSlug}'`, `export const GROUP_SLUG: SlugGroup = '${newGroupSlug}'`)
+  const updatedData = data.replace(/export const GROUP_SLUG: SlugGroup = '.*?'/, `export const GROUP_SLUG: SlugGroup = '${newGroupSlug}'`)
 
   fs.writeFile(constantsFilePath, updatedData, 'utf8', (err) => {
     if (err) {
@@ -22,69 +21,81 @@ fs.readFile(constantsFilePath, 'utf8', (err, data) => {
 
     console.log('Constants file updated successfully')
 
-    // Bước 2: Tìm kiếm và thay đổi lại hết tất cả trong source cụm từ "sdk-v2-egglepasskeywallet-decard" thành "egglepasskeywallet"
-    const directoryPath = path.join(__dirname, 'lib')
-    const filesToUpdate = [
-      path.join(__dirname, 'README.md'),
-      path.join(__dirname, 'package.json'),
-    ]
-    const searchString = 'sdk-v2-egglepasskeywallet-decard'
-    const replaceString = 'sdk-v2-egglepasskeywallet'
-
-    function replaceInFile (filePath) {
-      fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-          return console.log(err)
-        }
-        const result = data.replace(new RegExp(searchString, 'g'), replaceString)
-        fs.writeFile(filePath, result, 'utf8', (err) => {
-          if (err) return console.log(err)
-        })
-      })
-    }
-
-    function traverseDirectory (directory) {
-      fs.readdir(directory, (err, files) => {
-        if (err) {
-          return console.log('Unable to scan directory: ' + err)
-        }
-        files.forEach((file) => {
-          const filePath = path.join(directory, file)
-          fs.stat(filePath, (err, stat) => {
-            if (err) {
-              return console.log(err)
-            }
-            if (stat.isDirectory()) {
-              traverseDirectory(filePath)
-            } else {
-              replaceInFile(filePath)
-            }
-          })
-        })
-      })
-    }
-
-    traverseDirectory(directoryPath)
-    filesToUpdate.forEach(replaceInFile)
-
-    // Bước 3: Chạy lệnh yarn build
-    const { exec } = require('child_process')
-    exec('yarn build', (err, stdout, stderr) => {
+    // Bước 2: Lấy giá trị của name từ package.json và sử dụng nó làm searchString
+    const packageJsonPath = path.join(__dirname, 'package.json')
+    fs.readFile(packageJsonPath, 'utf8', (err, data) => {
       if (err) {
-        console.error('Error executing yarn build:', err)
+        console.error('Error reading package.json:', err)
         return
       }
-      console.log('Build completed successfully')
-      console.log(stdout)
-      console.log(stderr)
-      exec('yarn publish', (err, stdout, stderr) => {
+
+      const packageJson = JSON.parse(data)
+      const searchString = packageJson.name
+      const replaceString = `sdk-v2-${newGroupSlug}`
+
+      const directoryPath = path.join(__dirname, 'lib')
+      const filesToUpdate = [
+        path.join(__dirname, 'README.md'),
+        packageJsonPath,
+      ]
+
+      function replaceInFile (filePath) {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+          if (err) {
+            return console.log(err)
+          }
+          const result = data.replace(new RegExp(searchString, 'g'), replaceString)
+          fs.writeFile(filePath, result, 'utf8', (err) => {
+            if (err) return console.log(err)
+          })
+        })
+      }
+
+      function traverseDirectory (directory) {
+        fs.readdir(directory, (err, files) => {
+          if (err) {
+            return console.log('Unable to scan directory: ' + err)
+          }
+          files.forEach((file) => {
+            const filePath = path.join(directory, file)
+            fs.stat(filePath, (err, stat) => {
+              if (err) {
+                return console.log(err)
+              }
+              if (stat.isDirectory()) {
+                traverseDirectory(filePath)
+              } else {
+                replaceInFile(filePath)
+              }
+            })
+          })
+        })
+      }
+
+      traverseDirectory(directoryPath)
+      filesToUpdate.forEach(replaceInFile)
+
+      // Bước 3: Chạy lệnh yarn build
+      const { exec } = require('child_process')
+      exec('yarn build', (err, stdout, stderr) => {
         if (err) {
-          console.error('Error executing yarn publish:', err)
+          console.error('Error executing yarn build:', err)
           return
         }
-        console.log('Publish completed successfully')
+        console.log('Build completed successfully')
         console.log(stdout)
         console.log(stderr)
+
+        // Bước 4: Chạy lệnh yarn publish
+        exec('yarn publish', (err, stdout, stderr) => {
+          if (err) {
+            console.error('Error executing yarn publish:', err)
+            return
+          }
+          console.log('Publish completed successfully')
+          console.log(stdout)
+          console.log(stderr)
+        })
       })
     })
   })
