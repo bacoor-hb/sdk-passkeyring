b81e0ec4887de703b431665f71c0a499e3445c77
phamhugducBacoor <phamhungduc@bacoor.co>
2024-12-23 17:10:36 +0700

    Update comment in type.ts to clarify restrictions on adding types in SlugGroup

--- lib/constants/type.ts
+++ lib/constants/type.ts
@@ -1,4 +1,4 @@
-// must not add another type in this file, only add slug into SlugGroup=> because it related to file build.js, multi-build.js
+//! must not add another type in this file, only add slug into SlugGroup=> because it related to file build.js, multi-build.js
 export type SlugGroup =
   | 'egglegamewallet'
   | 'egglepasskeywallet'
