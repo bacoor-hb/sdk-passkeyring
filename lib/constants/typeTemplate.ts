8f887c867f336e628ae1cfc03a8b71201ed116f8
phamhugducBacoor <phamhungduc@bacoor.co>
2024-12-25 13:24:43 +0700

    Add new slugs to SlugGroup for meteornrun and meteornrun-decard

--- lib/constants/typeTemplate.ts
+++ lib/constants/typeTemplate.ts
@@ -1,11 +1,13 @@
 //! must not add another type in this file, only add slug into SlugGroup=> because it related to file build.js, multi-build.js
- type SlugGroup =
+type SlugGroup =
   | 'egglegamewallet'
   | 'egglepasskeywallet'
   | 'bacoorpasskeywallet'
   | 'passkeywallet'
   | 'keyringpasskeywallet'
-  | 'egglepasskeywallet-decard';
+  | 'egglepasskeywallet-decard'
+  | 'meteornrun-decard'
+  | 'meteornrun';
 
 const defaultExport = () => {}
 export default defaultExport
