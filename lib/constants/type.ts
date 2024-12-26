52ff9274ec4956716399c9f005e8147d46a4537f
phamhugducBacoor <phamhungduc@bacoor.co>
2024-12-26 09:34:37 +0700

    Reorder slug in SlugGroup type definition to maintain consistency

--- lib/constants/type.ts
+++ lib/constants/type.ts
@@ -1,6 +1,6 @@
-//! must not add another type in this file, only add slug into SlugGroup=> because it related to file build.js, multi-build.js
+//! must not add another type in this file, only add slug into SlugGroup=> because it related to file build.js, multi-build.js, please let "keyringpasskeywallet" at the end of the list
 export type SlugGroup =
   | 'egglegamewallet'
   | 'egglepasskeywallet'
-  | 'keyringpasskeywallet'
-  | 'egglepasskeywallet-decard';
+  | 'egglepasskeywallet-decard'
+  | 'keyringpasskeywallet';
