dad4599ba21bb79891ffa0a5032363c96e1b906d
phamhugducBacoor <phamhungduc@bacoor.co>
2025-03-12 16:18:42 +0700

    Rename package and update references from sdk-v2-egglegamewallet to sdk-v2-cyberstepwallet

--- src/config/appKit.js
+++ src/config/appKit.js
@@ -2,7 +2,7 @@ import { polygon } from '@reown/appkit/networks'
 import { createAppKit } from '@reown/appkit'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 // This import is required to inject global html tag into the app
-import { PasskeyProviderJS } from "sdk-v2-egglegamewallet";
+import { PasskeyProviderJS } from "sdk-v2-cyberstepwallet";
 
 
 const projectId = import.meta.env.VITE_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694" // this is a public projectId only to use on localhost
@@ -10,7 +10,7 @@ if (!projectId) {
   throw new Error('VITE_PROJECT_ID is not set')
 }
 
-// supported networks by sdk-v2-egglegamewallet: mainnet, optimism, bsc, polygon, arbitrum
+// supported networks by sdk-v2-cyberstepwallet: mainnet, optimism, bsc, polygon, arbitrum
 export const networks = [polygon]
 
 //Set up the Wagmi Adapter (Config)
