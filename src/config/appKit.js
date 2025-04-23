0d9c99246e1039dbc02521ce4823160ab1f2b85f
phamhungducBacoor <phamhungduc@bacoor.co>
2025-04-23 13:58:36 +0700

    Update sdk-v2-meteornrun to version 2.6.0 and remove deprecated PasskeyProviderJS import

--- src/config/appKit.js
+++ src/config/appKit.js
@@ -2,7 +2,8 @@ import { polygon } from '@reown/appkit/networks'
 import { createAppKit } from '@reown/appkit'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 // This import is required to inject global html tag into the app
-import { PasskeyProviderJS } from "sdk-v2-meteornrun";
+// import { PasskeyProviderJS } from "sdk-v2-meteornrun"; // used for version <2.6.0
+import { createPasskeyProvider } from "sdk-v2-meteornrun"; // used for version >= 2.6.0
 
 
 const projectId = import.meta.env.VITE_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694" // this is a public projectId only to use on localhost
@@ -26,3 +27,4 @@ export const appKit = createAppKit({
   enableEIP6963: true,
   enableInjected: true,
 })
+createPasskeyProvider()
